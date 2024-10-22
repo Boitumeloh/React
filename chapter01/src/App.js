ca962ce348301e34555a350422026313f3fcae51
Boitumelo <boitumelolefophane@gmail.com>
2024-10-22 15:15:06 +0200

    chapter01

--- chapter01/src/App.js
+++ chapter01/src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import React, { Component } from "react";
 class App extends Component {
   render() {
-    return (
+    return ( 
       <div>
         <h1>My First React App!</h1>
       </div>
