985de1189f342d1135e105cd4a11239e453df041
Boitumelo <boitumelolefophane@gmail.com>
2024-10-23 10:50:03 +0200

    chapter 07

--- chapter06/src/App.js
+++ chapter06/src/App.js
@@ -1,16 +1,15 @@
 import logo from "./logo.svg";
 import React, { Component } from "react";
-import Rating from './Rating';
-import Products from "./Products";
-import JumboTronComponent from "./JumboTronComponent";
+// import Rating from './Rating';
+// import Products from "./Products";
+// import JumboTronComponent from "./JumboTronComponent";
+import UserForm from "./UserForm";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Products/> */}
-        <JumboTronComponent>This is a simple hero unit, a simple jumbotron-style component for
-        calling extra attention to featured content or information.</JumboTronComponent>
+        <UserForm />
       </div>
     );
   }
--- chapter06/src/UserForm.js
+++ chapter06/src/UserForm.js
@@ -20,10 +20,19 @@ class UserForm extends Component {
               !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
             ) {
               errors.email = "Invalid email address";
+            } else if (values.email.length < 10) {
+              errors.email = "Email address too short";
+            }
+            if (!values.password) {
+              errors.password = "Required";
+            } else if (values.password.length < 8) {
+              errors.password = "Password too short";
             }
             return errors;
           }}
+          //once submit button is clicked
           onSubmit={(values, { setSubmitting }) => {
+            //delays alert by 400ms
             setTimeout(() => {
               alert(JSON.stringify(values, null, 2));
               setSubmitting(false);
@@ -33,9 +42,14 @@ class UserForm extends Component {
           {({ isSubmitting }) => (
             <Form>
               <Field type="email" name="email" />
+              <span style = {{color: "red", fontWeight:"bold"}}>
+
               <ErrorMessage name="email" component="div" />
+              </span>
               <Field type="password" name="password" />
+              <span style = {{color: "red", fontWeight:"bold"}}>
               <ErrorMessage name="password" component="div" />
+              </span>
               <button type="submit" disabled={isSubmitting}>
                 Submit
               </button>
